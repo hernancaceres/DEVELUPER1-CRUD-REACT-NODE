d211fba0611b1f6823736c3db370e07d16b20c63
hernancaceres <harinatha777@hotmail.com>
2023-12-26 23:29:08 -0300

    crear pubicacion y traer publicacionesfuncionando en el back

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 export const sequelize = new Sequelize(
     "projectsdb1", // db name,
     "postgres", // username
-    "v4juuK4fUPYoguaK", // password
+    "password", // password
     {
         host: "localhost",
         dialect: "postgres",
