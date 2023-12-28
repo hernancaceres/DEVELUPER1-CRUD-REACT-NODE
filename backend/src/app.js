d84a8254a63a991347f507a8bfad1457a82c2630
hernancaceres <harinatha777@hotmail.com>
2023-12-27 23:08:07 -0300

    instalacion de cors en el back

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import {FRONTEND_URL} from "./config.js"
 
 const app = express();
 
@@ -9,6 +11,11 @@ import publicacionRoutes from "./routes/publicacion.routes.js";
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(cors({
+    origin: FRONTEND_URL,  // Reemplaza con la URL de tu aplicación de React
+    credentials: true,
+}));
 
 // Routes
 app.get("/usuarios", (req, res) => {
